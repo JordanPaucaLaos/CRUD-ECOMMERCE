bfaf8ac71ac0ef84ed2883934e05fe644636b24e
jordanxto <jesus_pl_16@hotmail.com>
2023-08-13 21:29:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+
+const indexRouter = require('./src/routes/index');
+const tiendaRouter = require('./src/routes/tiendaRoutes');
+const blogRouter = require('./src/routes/blogRoutes');
+const acercaRouter = require('./src/routes/acercaRoutes');
+const contactoRouter = require('./src/routes/contactoRoutes');
+
+
+
+// view engine setup
+app.set('views', path.resolve(__dirname, './src/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.use('/', indexRouter);
+app.use(tiendaRouter);
+app.use(blogRouter);
+app.use(acercaRouter);
+app.use(contactoRouter);
+
+
+app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
