a2b53287b89348e19e450cb35ee1930727961406
jordanxto <jesus_pl_16@hotmail.com>
2023-08-23 18:37:19 -0500

    update bd

--- app.js
+++ app.js
@@ -27,38 +27,6 @@ app.use(blogRouter);
 app.use(acercaRouter);
 app.use(contactoRouter);
 
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and Re-sync db.");
-    db.Marca.bulkCreate([
-        { nombre_marca: "Nike" },
-        { nombre_marca: "Adidas" },
-        { nombre_marca: "Reebok" },
-        { nombre_marca: "Fila" }
-    ]).then(() => console.log("Datos de marcas han sido guardados!!"));
 
-    db.Producto.bulkCreate([
-        {
-            nombre_prod: "Zapatilla 1",
-            precio_prod: 245.50,
-            id_marca: 1,
-            nombre_imagen: "zapatilla_default.jpg"
-        },
-        {
-            nombre_prod: "Zapatilla 2",
-            precio_prod: 345.50,
-            id_marca: 2,
-            nombre_imagen: "zapatilla_default.jpg"
-        },
-        {
-            nombre_prod: "Zapatilla 3",
-            precio_prod: 145.50,
-            id_marca: 3,
-            nombre_imagen: "zapatilla_default.jpg"
-        }
-
-    ]).then(() => console.log("Datos de productos han sido guardados!!"));
-
-});
 
 app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
--- base de datos/bd_tienda.sql
+++ base de datos/bd_tienda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-08-2023 a las 05:54:25
+-- Tiempo de generación: 24-08-2023 a las 01:31:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `marcas` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` bigint(11) UNSIGNED NOT NULL,
   `nombre_marca` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -38,7 +38,9 @@ CREATE TABLE `marcas` (
 
 INSERT INTO `marcas` (`id`, `nombre_marca`) VALUES
 (1, 'Nike'),
-(2, 'Adidas');
+(2, 'Adidas'),
+(3, 'Reebok'),
+(4, 'Fila');
 
 -- --------------------------------------------------------
 
@@ -47,10 +49,10 @@ INSERT INTO `marcas` (`id`, `nombre_marca`) VALUES
 --
 
 CREATE TABLE `productos` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` bigint(11) UNSIGNED NOT NULL,
   `nombre_prod` varchar(100) NOT NULL,
-  `precio_prod` decimal(7,2) NOT NULL,
-  `id_marca` int(11) UNSIGNED DEFAULT NULL,
+  `precio_prod` decimal(7,2) UNSIGNED NOT NULL,
+  `id_marca` bigint(11) UNSIGNED DEFAULT NULL,
   `nombre_imagen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -59,29 +61,9 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre_prod`, `precio_prod`, `id_marca`, `nombre_imagen`) VALUES
-(1, 'Retro fly 6', 450.25, 1, ''),
-(2, 'Nemesis 5', 350.40, 2, ''),
-(3, 'yoel', 120.50, 1, ''),
-(4, 'kingston', 6786.00, 1, 'zapatilla_default.jpg'),
-(5, 'jesus', 120.00, 1, 'zapatilla_default.jpg'),
-(6, 'Katakuri', 123.00, 1, 'zapatilla_default.jpg'),
-(7, 'w', 1.00, 1, 'zapatilla_default.jpg'),
-(8, 'z', 34.00, 1, 'foto jesus.png'),
-(9, 'sasa', 132.00, 1, 'zapatilla_default.jpg');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nombres` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `celular` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(1, 'Zapatilla 1', 245.50, 1, 'zapatilla_default.jpg'),
+(2, 'Zapatilla 2', 345.50, 2, 'zapatilla_default.jpg'),
+(3, 'Zapatilla 3', 145.50, 3, 'zapatilla_default.jpg');
 
 --
 -- Índices para tablas volcadas
@@ -101,12 +83,6 @@ ALTER TABLE `productos`
   ADD KEY `id_marca` (`id_marca`);
 
 --
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -114,19 +90,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -136,7 +106,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`);
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
